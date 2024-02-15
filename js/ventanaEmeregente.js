6d7e2e0da2acef4425a2241a2e2a0bef1c37ff57
LTAPIAR <luistapiariz17@gmail.com>
2024-02-15 11:39:38 -0500

    corrigiendo problemas de carga minificacion de archivos , optimicion de img y diferencias las carga importante

--- js/ventanaEmeregente.js
+++
@@ -1,9 +0,0 @@
-let btnSi =document.getElementById("btnSi");
-let containerPopUp =document.getElementById("containerPopUp");
-let cerrarPopUp =document.getElementById("cerrarPopUp");
-btnSi.addEventListener("click", function(){
-    containerPopUp.style.display ="flex";
-});
-cerrarPopUp.addEventListener("click", function(){
-    containerPopUp.style.display ="none";
-});
